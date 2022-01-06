0f4afe4f0c298c8cf71822dcb0910be848fdc901
Denis Stepanov <denis.s.stepanov@oracle.com>
2022-01-06 16:12:48 +0700

    Progress

--- data-tck/src/main/java/io/micronaut/data/tck/entities/Country.java
+++ data-tck/src/main/java/io/micronaut/data/tck/entities/Country.java
@@ -15,8 +15,10 @@
  */
 package io.micronaut.data.tck.entities;
 
-import io.micronaut.data.annotation.*;
-import org.bson.codecs.pojo.annotations.BsonId;
+import io.micronaut.data.annotation.AutoPopulated;
+import io.micronaut.data.annotation.Id;
+import io.micronaut.data.annotation.MappedEntity;
+import io.micronaut.data.annotation.Relation;
 
 import java.util.Set;
 import java.util.UUID;
@@ -24,7 +26,6 @@ import java.util.UUID;
 @MappedEntity
 public class Country {
 
-    @BsonId
     @Id
     @AutoPopulated
     private UUID uuid;
